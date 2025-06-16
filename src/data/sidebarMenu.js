40c4999dd1d17c30d32670028645eff689ddafd8
JDubas94 <uiadubas94@gmail.com>
2025-06-16 17:34:17 +0300

    add sidebarMenu data

---
+++ src/data/sidebarMenu.js
@@ -0,0 +1,11 @@
+const sidebarMenu = [
+  { name: "Медикаменты", slug: "medications" },
+  { name: "Витамины и минералы", slug: "vitamins" },
+  { name: "Спорт и здоровье", slug: "sports-health" },
+  { name: "Лечебно-профилактические средства", slug: "preventive" },
+  { name: "Дерматокосметика", slug: "dermatocosmetics" },
+  { name: "Красота и уход", slug: "beauty-care" },
+  { name: "Товары для детей и мам", slug: "mothers-kids" },
+]; 
+
+export default sidebarMenu;
